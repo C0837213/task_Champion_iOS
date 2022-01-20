bf574aa5fd35f418aaaa91cb359b932782945303
Philip Chau <kyoc2020@gmail.com>
2022-01-20 18:26:39 -0500

    Initial Commit

---
+++ task_Champion_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_Champion_iOS
+//
+//  Created by Philip Chau on 20/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
