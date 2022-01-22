030ebdf94c7238ed4641bc8fa31d7215f89bfab7
Onurcan Sever <onsever@icloud.com>
2022-01-21 19:47:20 -0500

    added homescreen viewcontroller

---
+++ task_Champion_iOS/Controller/HomeScreenVC.swift
@@ -0,0 +1,21 @@
+//
+//  HomeScreenVC.swift
+//  task_Champion_iOS
+//
+//  Created by Onurcan Sever on 2022-01-21.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
