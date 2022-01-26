f8238db37055ff149da2961229e75b73e8dae113
Onurcan Sever <onsever@icloud.com>
2022-01-26 15:00:33 -0500

    fixed updating homescreenvc when category changes and completed category screen

--- task_Champion_iOS/Controller/CategoryTVC.swift
+++ task_Champion_iOS/Controller/CategoryTVC.swift
@@ -6,64 +6,164 @@
 //
 
 import UIKit
-/*
+import CoreData
+
 class CategoryTVC: UITableViewController {
 
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    private var categories = [Category]()
+    public weak var delegate: HomeScreenVC?
+    public var categories = [Category]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Categories"
+        loadCategories()
+        view.backgroundColor = .crystalWhite
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewCategory))
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        delegate?.updateCategories(categories: self.categories)
+        delegate?.updateViews()
+    }
+    
+    @objc private func addNewCategory() {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add a Category", message: "Please name your category", preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            textField = field
+        }
+        
+        let okAction = UIAlertAction(title: "Add", style: .default) { action in
+            
+            guard let text = textField.text else {
+                return
+            }
+            
+            if text.count > 1 {
+                let newCategory = Category(context: self.context)
+                newCategory.name = text
+                self.categories.append(newCategory)
+                
+                self.saveData()
+                self.loadCategories()
+                
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { action in
+            
+            self.loadCategories()
+                        
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+        
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let item = models[indexPath.row]
-        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
-        sheet.addAction(UIAlertAction(title: "Cancle", style: .cancel, handler: nil))
-        sheet.addAction(UIAlertAction(title: "Edit", style: .default , handler: {
-            _ in
-            let alert = UIAlertController(title: "Edit Item", message: "Edit Category", preferredStyle: .alert)
-            alert.addTextField(configurationHandler: nil)
-            alert.textFields?.first?.text=item.name
-            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: {[weak self] _ in
-                guard let field = alert.textFields?.first,let newName = field.text, !newName .isEmpty else{
-                    return
-                }
-                self?.updateItem(item: item, newName: newName)
-            }))
-            self.present(alert,animated: true)
-        
-        }))
-        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler:{ [weak self]_ in
-       
-            self?.deleteItem(item: item)
-        } ))
-        present(sheet,animated: true)
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return models.count
+        return categories.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = model.name
+        cell.textLabel?.text = categories[indexPath.row].name
+        cell.backgroundColor = .crystalWhite
 
         return cell
     }
     
-
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { action, view, completion in
+            
+            self.context.delete(self.categories[indexPath.row])
+            self.saveData()
+            self.categories.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            tableView.reloadData()
+            
+            completion(true)
+            
+        }
+        
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { action, view, completion in
+            
+            var textField = UITextField()
+            let alert = UIAlertController(title: "Edit Category", message: "Please enter a name to change the category name", preferredStyle: .alert)
+            
+            alert.addTextField { field in
+                textField = field
+            }
+            
+            textField.text = self.categories[indexPath.row].name
+            
+            let changeAction = UIAlertAction(title: "Edit", style: .default) { action in
+                
+                guard let text = textField.text else { return }
+                
+                self.categories[indexPath.row].name = text
+                
+                self.saveData()
+                self.loadCategories()
+                self.delegate?.updateViews()
+                
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { action in
+                
+                self.loadCategories()
+                
+            }
+            
+            alert.addAction(changeAction)
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion: nil)
+            completion(true)
+            
+        }
+        
+        editAction.backgroundColor = .systemGray2
+        
+        let configuration = UISwipeActionsConfiguration(actions: [editAction, deleteAction])
+        
+        return configuration
+        
+    }
+    
+    private func saveData() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context... \(error.localizedDescription)")
+        }
+    }
+    
+    private func loadCategories() {
+        let request:NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            self.categories = try context.fetch(request)
+        } catch {
+            print("Error load categories ... \(error.localizedDescription)")
+        }
+        
+        tableView.reloadData()
+    }
 
 }
-
-*/
